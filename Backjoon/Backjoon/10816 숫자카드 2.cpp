0e82692dde50153b71f1357abafe8550c18dd0ca
김태양 <xodid0936@gmail.com>
2019-09-08 17:14:21 +0900

    10816 Binary search - but, TLE.  so, use bound(upper, lower)

---
+++ Backjoon/Backjoon/10816 숫자카드 2.cpp
@@ -0,0 +1,109 @@
+//#include <iostream>
+//#include <algorithm>
+//
+//typedef long long ll;
+//using namespace std;
+//
+//ll card[500000];
+//ll find_c[500000];
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(0); cout.tie(0);
+//
+//	int N, M;
+//	int result = 0;
+//	cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> card[i];
+//	}
+//
+//	cin >> M;
+//
+//	for (int i = 0; i < M; i++)
+//	{
+//		cin >> find_c[i];
+//	}
+//
+//	sort(card, card + N);
+//
+//	for (int i = 0; i < M; i++)
+//	{
+//
+//		int left = 0, right = N - 1;
+//
+//		while ((card[left] != card[right]) && (left<right) )
+//		{
+//			int mid = (left + right) / 2;
+//
+//			if (card[mid] > find_c[i])
+//			{
+//				right = mid - 1;
+//			}
+//			else if (card[mid] == find_c[i])
+//			{
+//				if (card[left] == card[right] && card[left] == find_c[i]) break;
+//
+//				if (card[left] != find_c[i])
+//				{
+//					if (left > 1)left--;
+//					else left++;
+//				}
+//				if (card[right] != find_c[i])
+//				{
+//					if (right < N - 1)right++;
+//					else right--;
+//				}
+//				
+//			}
+//			else
+//			{
+//				left = mid + 1;
+//			}
+//		}
+//
+//		if ((card[left] == card[right]) && (card[left] == find_c[i])) result = right - left + 1;
+//		else if (right - left < 0) result = 0;
+//		else
+//			result = right - left;
+//
+//
+//		cout << result << " ";
+//	}
+//	return 0;
+//}
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int N, M, findNum;
+
+	cin >> N;
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		cin >> findNum;
+
+		cout << upper_bound(v.begin(), v.end(), findNum) - lower_bound(v.begin(), v.end(), findNum) << " ";
+	}
+
+	return 0;
+}
