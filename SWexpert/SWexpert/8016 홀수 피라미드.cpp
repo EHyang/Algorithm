00be2a0ed8b697cce8df066731b98ea63c1d5d1e
김태양 <xodid0936@gmail.com>
2019-10-22 18:26:51 +0900

    8016 math

---
+++ SWexpert/SWexpert/8016 홀수 피라미드.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int t;
+	cin >> t;
+	int testcase = 1;
+
+	while (t--)
+	{
+		ll input;
+		cin >> input;
+
+		ll left=1, right=1;
+
+		if (input != 1)
+		{
+			left += (input - 1)*(4 + 4 * (input - 2)) / 2;
+			right = left + (4 * (input - 1));
+		}
+		
+		cout <<"#"<<testcase++<<" "<< left << " " << right << endl;
+	}
+
+	return 0;
+}
